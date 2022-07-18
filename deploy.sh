dac418a26fc0ebdb82e3bd12b9dccfeed66bf53b
Rekord <1324596506@qq.com>
2022-07-18 22:05:47 +0800

    refactor: use function to refactor script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,28 @@
 
 # This script is used to deploy my custom content.
 
+deleteMDFromPublic() {
+    find $1 -name "*.md" > md
+    while read line
+    do
+        rm "$line"
+    done < md
+    rm md
+}
+
+# exist some bugs
+# md to html
+mgToHtml() {
+    go install github.com/ueffel/mdtohtml@latest
+    find $1 -name "*.md" > md
+    while read line
+    do
+        mdtohtml "$line"
+    done < md
+    rm md
+}
+
+
 rm -rf public/nginx_zh
 rm -rf public/music_theory
 rm -rf public/personal
@@ -10,27 +32,16 @@ if [ ! -e "public" ]; then
     mkdir public
 fi
 
-cp -rf personal/nginx_zh public/nginx_zh
-
-# md to html
-# go install github.com/ueffel/mdtohtml@latest
-# find personal/music_theory -name "*.md" > md
-# while read line
-# do
-#     mdtohtml "$line"
-# done < md
-# rm md
-
 # copy personal/music_theory/*.html to public/music_theory
 # find personal/music_theory \! -name "index.html" > html
+
+cp -rf personal/nginx_zh public/nginx_zh
 cp -rf personal/music_theory public/music_theory
+cp -rf personal/lao public/lao
+
 # delete all .md file
-find public/music_theory -name "*.md" > md
-while read line
-do
-    rm "$line"
-done < md
-rm md
+deleteMDFromPublic "public/music_theory"
+deleteMDFromPublic "public/lao"
 
 
 # delete all *.Identifier
