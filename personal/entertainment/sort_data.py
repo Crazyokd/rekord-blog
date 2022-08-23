1fe999a26cee131b44b9952a7aa6af5ca749b34c
Rekord <1324596506@qq.com>
2022-08-23 20:03:05 +0800

    chore: prompt whether repeat

--- personal/entertainment/sort_data.py
+++ personal/entertainment/sort_data.py
@@ -8,6 +8,16 @@ import json
 with open('data.json', encoding='utf-8') as f:
     json_datas = json.load(f)
 
+# 去重
+ser_list = []
+for video in json_datas:
+    ser_list.append(video['ser'])
+origin_len = len(ser_list)
+current_len = len(list(set(ser_list)))
+
+if origin_len != current_len:
+    print('存在重复视频')
+
 def takeSer(video):
     return video['ser']
 
